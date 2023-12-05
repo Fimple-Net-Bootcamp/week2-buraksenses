47ff09dfe2bf7b05cd6bc3fff9d838bb3af5718d
Burak Şenses <buraksenseees@gmail.com>
2023-12-05 16:09:03 +0300

    directories created.

---
+++ WeatherApp/WeatherApp.API/Data/WeatherAppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WeatherApp.API.Models.Domains;
+
+namespace WeatherApp.API.Data;
+
+public class WeatherAppDbContext : DbContext
+{
+    public WeatherAppDbContext(DbContextOptions options) : base(options)
+    {
+        
+    }
+
+    public DbSet<Satellite> Satellites { get; set; }
+
+    public DbSet<Planet> Planets { get; set; }
+}
---
+++ WeatherApp/WeatherApp.API/Models/Domains/Planet.cs
@@ -0,0 +1,6 @@
+﻿namespace WeatherApp.API.Models.Domains;
+
+public class Planet
+{
+    
+}
